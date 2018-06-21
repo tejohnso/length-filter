d10c5625496be66c3f5379e2371979a38db662d6
Tyler Johnson <mail@tyler-johnson.ca>
2018-06-21 18:54:11 -0400

    Filter on lengths

--- flen.cpp
+++ flen.cpp
@@ -1,11 +1,18 @@
 #include <string>
+#include <sstream>
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
+  string::size_type min = 0, max = numeric_limits<string::size_type>::max();
+
+  if (argc > 1) {min = atoi(argv[1]);}
+  if (argc > 2) {max = atoi(argv[2]);}
+
+  cout << min <<endl << max << endl;
   for (string word; getline(cin, word);) {
-    if (word.empty()) {continue;}
+    if (word.length() < min || word.length() > max) {continue;}
     cout << word << endl;
   }
 }
